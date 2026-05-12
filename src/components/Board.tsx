1f43e7ffe90a1fb50d96eb1e8a0d4660fc4d2a4a
Cesar Sevilla <cesar_sevilla@outlook.com>
2026-05-12 11:48:09 -0500

    feat: update Board component to handle LetterResult type and improve cell styling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,24 @@
-import type { GameStatus } from '../types'
+import type { GameStatus, LetterResult } from '../types'
 
 interface BoardProps {
     currentGuess: string;
-    historyGuess: string[];
+    historyGuess: LetterResult[][];
     MAX_WORDS: number;
     WORD_LENGTH: number;
     status: GameStatus;
 }
 
 export default function Board({ currentGuess, historyGuess, MAX_WORDS, WORD_LENGTH, status }: BoardProps) {
+
+    const getCellStyle = (letterStatus: LetterResult['status']) => {
+        if(letterStatus === "correct"){
+            return "bg-green-100 border-green-100"
+        } else if(letterStatus === "present"){
+            return "bg-yellow-100 border-yellow-100"
+        } else {
+            return "bg-gray-100 border-gray-100"
+        }
+    }
     
     return (
         <div>
@@ -22,8 +32,8 @@ export default function Board({ currentGuess, historyGuess, MAX_WORDS, WORD_LENG
                                     const isActiveRow = x === historyGuess.length && status === 'playing';
                                     const isPastRow = x < historyGuess.length;
                                     return(
-                                        <div key={y} className={`w-15 h-15 mt-2 border-2 flex text-lg items-center justify-center ${isActiveRow ? 'bg-gray-100' : isPastRow ? 'bg-gray-100 border-gray-100' : 'bg-gray-400 border-gray-400'}`}>
-                                            { historyGuess[x] ? historyGuess[x][y] : isActiveRow ? currentGuess[y] : '' }
+                                        <div key={y} className={`w-15 h-15 mt-2 border-2 flex text-lg items-center justify-center ${isPastRow ? getCellStyle(historyGuess[x][y].status) : ''} ${isActiveRow ? 'bg-gray-100' : !isPastRow ? 'bg-gray-400 border-gray-400' : ''}`}>
+                                            { isPastRow ? historyGuess[x][y].letter : isActiveRow ? currentGuess[y] : '' }
                                         </div>
                                     )
                                 })
