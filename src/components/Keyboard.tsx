9a8e110cae15720fc31d9504c67d9cd1dd4ecc50
Cesar Sevilla <cesar_sevilla@outlook.com>
2026-05-13 18:12:41 -0500

    refactor: standardize formatting and improve button click handling in Keyboard component

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,44 +1,50 @@
-import type { LetterResult } from '../types'
+import type { LetterResult } from "../types";
 
-const STATUS_CLASS: Record<LetterResult['status'], string> = {
-    correct: 'tile-correct',
-    present: 'tile-present',
-    absent: 'tile-absent',
-}
+const STATUS_CLASS: Record<LetterResult["status"], string> = {
+  correct: "tile-correct",
+  present: "tile-present",
+  absent: "tile-absent",
+};
 
 interface KeyboardProps {
-    letterStatuses: Record<string, LetterResult['status']>
-    handleButtonClick: (letter: string) => void;
+  letterStatuses: Record<string, LetterResult["status"]>;
+  handleButtonClick: (letter: string) => void;
 }
 
-export default function Keyboard({ letterStatuses, handleButtonClick }: KeyboardProps) {
+export default function Keyboard({
+  letterStatuses,
+  handleButtonClick,
+}: KeyboardProps) {
+  const keys = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
+  ];
 
-    const keys = [
-        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-        ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace']
-    ]
-
-    return (
-        <div>
-            {
-                keys.map((row, indexRow) => {
-                    return(
-                        <div key={indexRow} className="flex gap-2 justify-center mx-2">
-                            {
-                                row.map((letter, indexLetter) => {
-                                    const status = letterStatuses[letter]
-                                    return(
-                                        <button onClick={() => handleButtonClick(letter)} key={indexLetter} className={`key w-15 h-15 mt-2 rounded-sm flex text-lg font-semibold items-center justify-center ${status ? STATUS_CLASS[status] : ''}`}>
-                                            {letter === 'Backspace' ? '⌫' : letter === 'Enter' ? '↵' : letter}
-                                        </button>
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  return (
+    <div>
+      {keys.map((row, indexRow) => {
+        return (
+          <div key={indexRow} className="flex gap-2 justify-center mx-2">
+            {row.map((letter, indexLetter) => {
+              const status = letterStatuses[letter];
+              return (
+                <button
+                  tabIndex={-1}
+                  onClick={(e) => {
+                    (e.currentTarget as HTMLButtonElement).blur();
+                    handleButtonClick(letter);
+                  }}
+                  key={indexLetter}
+                  className={`key w-15 h-15 mt-2 rounded-sm flex text-lg font-semibold items-center justify-center ${status ? STATUS_CLASS[status] : ""} ${letter === "ENTER" ? "w-25 text-xs" : ""} ${letter === "Backspace" ? "w-20" : ""}  `}
+                >
+                  {letter === "Backspace" ? "⌫" : letter}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
